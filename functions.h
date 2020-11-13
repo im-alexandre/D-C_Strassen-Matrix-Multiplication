eedc1ee5daee4e1189068267923257f89a1dd35a
luisgaboardi <luis.ggaboardi@gmail.com>
2020-11-13 14:52:41 -0300

    Implemented strassen algorithm and adapted code to it

--- functions.h
+++ functions.h
@@ -3,97 +3,73 @@
 #include <time.h>
 #include <unistd.h>
 
-int size; // Tamanho da matriz
-int **matrix_A;
-int **matrix_B;
-int **result_matrix;
+int **A;
+int **B;
+int **C;
 
-void Config();
-int determinantOfMatrix(int **mat, int n);
+void printMatrix(int **M, int n);
 
-void Menu1()
-{
-	system("clear");
-	printf("Multiplicação de Matrizes Quadradas\n\n");
-
-	printf("1) Configurar matrizes\n");
-	printf("2) Sair\n\n");
-	printf(">> ");
-
-	int entrada;
-	scanf("%d", &entrada);
-
-	switch(entrada)
-	{
-		case 1:
-			Config();
-			break;
-
-		case 2:
-			exit(EXIT_SUCCESS);
-			break;
-	}
-
-}
-
-void Menu2()
-{
-	system("clear");
-	printf("Multiplicação de Matrizes Quadradas\n\n");
-
-	printf("1) Prosseguir com a execução\n");
-	printf("2) Sair\n\n");
-	printf(">> ");
-
-	int entrada;
-	scanf("%d", &entrada);
-
-	switch(entrada)
-	{
-		case 1:
-			//Multiply();
-			break;
-
-		case 2:
-			exit(EXIT_SUCCESS);
-			break;
-	}
-
-}
-
-void allocMatrixes()
+int **initializeMatrix(int n)
 {
 	//// Matrix A ////
-	matrix_A = (int **) malloc(size * sizeof(int *));
-	for(int i = 0; i < size; i++)
-		matrix_A[i] = (int *) malloc(size * sizeof(int));
+	int **temp = (int **) malloc(n * sizeof(int *));
+	for(int i = 0; i < n; i++)
+		temp[i] = (int *) malloc(n * sizeof(int));
+
+	return temp;
+}
+
+void allocMatrixes(int n)
+{
+	//// Matrix A ////
+	A = initializeMatrix(n);
+	B = initializeMatrix(n);
+	C = initializeMatrix(n);
 
 	srand(time(0));
 
-	for(int l = 0; l < size; l++)
-		for(int c = 0; c < size; c++)
-			matrix_A[l][c] = rand()%31;
-
-	//// Matrix B ////
-	matrix_B = (int **) malloc(size * sizeof(int *));
-	for(int i = 0; i < size; i++)
-		matrix_B[i] = (int *) malloc(size * sizeof(int));
-
-
-	for(int l = 0; l < size; l++)
-		for(int c = 0; c < size; c++)
-			matrix_B[l][c] = rand()%31;
-
-	//// Result Matrix ////
-	result_matrix = (int **) malloc(size * sizeof(int *));
-	for(int i = 0; i < size; i++)
-		result_matrix[i] = (int *) malloc(size * sizeof(int));
+	for(int l = 0; l < n; l++)
+	{
+		for(int c = 0; c < n; c++)
+		{
+			A[l][c] = rand()%31;
+			B[l][c] = rand()%31;
+		}
+	}
 }
 
-void printMatrix(int **M)
+int GenerateMatrixes()
 {
-	for(int l = 0; l < size; l++) {
-		for(int c = 0; c < size; c++) {
+	system("clear");
+
+	printf("Determine o tamanho N (par) das 2 matrizes N x N a serem geradas\n\n>> ");
+
+	int n;
+	scanf("%d", &n);
+
+	if(n % 2 != 0) { printf("Valor inválido! O algoritmo só funciona com N par.\n"); exit(EXIT_FAILURE); }
+
+	printf("\nSerão geradas 2 matrizes %d x %d de inteiros aleatórios\n", n, n);
+
+	allocMatrixes(n);
+	sleep(3);
+
+	system("clear");
+
+	printf("Matrizes geradas:\n\n");
+	printf("Matriz A\n");
+	printMatrix(A, n);
+	printf("Matriz B\n");
+	printMatrix(B, n);
+
+	return n;
+}
+
+
+void printMatrix(int **M, int n)
+{
+	for(int l = 0; l < n; l++) {
+		for(int c = 0; c < n; c++) {
 			printf("%d ", M[l][c]);
 		}
 		printf("\n");
@@ -102,37 +78,82 @@ void printMatrix(int **M)
 	
 }
 
-void Config()
+int** add(int **M1, int **M2, int n)
 {
-	system("clear");
-	printf("Qual será o tamanho N da matriz N x N?\n\n");
-	printf("(Pela limitação de espaço gráfico, a visualiação das matrizes\ne de seus resultados só estão disponíveis para valores de N <= 10)\n\n>> ");
-	
-	scanf("%d", &size);
-	
-	printf("\nSerá gerada uma matriz %d x %d de inteiros aleatórios...\n", size, size);
+    int **temp = initializeMatrix(n);
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            temp[i][j] = M1[i][j] + M2[i][j];
 
-	allocMatrixes();
-	sleep(3);
+    return temp;
+}
 
-	system("clear");
+int **subtract(int **M1, int **M2, int n)
+{
+    int **temp = initializeMatrix(n);
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            temp[i][j] = M1[i][j] - M2[i][j];
 
-	if(size <= 10)
-	{
-		printf("Matrizes Geradas\n\n");
-		printf("Matriz A\n\n");
-		printMatrix(matrix_A);
-		printf("Matriz B\n\n");
-		printMatrix(matrix_B);
+    return temp;
+}
+
+int **strassenMultiply(int **A, int **B, int n)
+{
+	int** C = initializeMatrix(n);
+	int k = n/2;
+
+  	if (n == 1) {
+    	int** C = initializeMatrix(1);
+    	C[0][0] = A[0][0] * B[0][0];
+    	return C;
 	}
 	else
 	{
-		printf("Dada o tamanho da matriz, será apresentado somente o elemento na posição [0,0]\n\n");
-		printf("Matriz A = %d\n"  , matrix_A[0][0]);
-		printf("Matriz B = %d\n\n", matrix_B[0][0]);
-	}
+		int **A11 = initializeMatrix(k);
+		int **A12 = initializeMatrix(k);
+		int **A21 = initializeMatrix(k);
+		int **A22 = initializeMatrix(k);
+		int **B11 = initializeMatrix(k);
+		int **B12 = initializeMatrix(k);
+		int **B21 = initializeMatrix(k);
+		int **B22 = initializeMatrix(k);
 
-	printf("Aperte Enter para prosseguir...\n");
-	getchar();
-	getchar();
+		for(int i=0; i<k; i++) {
+		    for(int j=0; j<k; j++) {
+		        A11[i][j] = A[i][j];
+		        A12[i][j] = A[i][k+j];
+		        A21[i][j] = A[k+i][j];
+		        A22[i][j] = A[k+i][k+j];
+		        B11[i][j] = B[i][j];
+		        B12[i][j] = B[i][k+j];
+		        B21[i][j] = B[k+i][j];
+		        B22[i][j] = B[k+i][k+j];
+	   		}
+		}
+
+		int** P1 = strassenMultiply(A11, subtract(B12, B22, k), k);
+		int** P2 = strassenMultiply(add(A11, A12, k), B22, k);
+		int** P3 = strassenMultiply(add(A21, A22, k), B11, k);
+		int** P4 = strassenMultiply(A22, subtract(B21, B11, k), k);
+		int** P5 = strassenMultiply(add(A11, A22, k), add(B11, B22, k), k);
+		int** P6 = strassenMultiply(subtract(A12, A22, k), add(B21, B22, k), k);
+		int** P7 = strassenMultiply(subtract(A11, A21, k), add(B11, B12, k), k);
+
+		int** C11 = subtract(add(add(P5, P4, k), P6, k), P2, k);
+		int** C12 = add(P1, P2, k);
+		int** C21 = add(P3, P4, k);
+		int** C22 = subtract(subtract(add(P5, P1, k), P3, k), P7, k);
+
+		for(int i = 0; i < k; i++) {
+		    for(int j = 0; j < k; j++) {
+		        C[i][j] = C11[i][j];
+		        C[i][j+k] = C12[i][j];
+		        C[k+i][j] = C21[i][j];
+		        C[k+i][k+j] = C22[i][j];
+		    }
+		}
+	
+		return C;
+	}
 }
--- main.c
+++ main.c
@@ -2,9 +2,27 @@
 
 int main(int argc, char const *argv[])
 {
+	int n;
+	system("clear");
+	printf("Multiplicação de Matrizes Quadradas\n\n");
 
-	Menu1();
-	Menu2();
+	printf("1) Gerar matrizes\n");
+	printf("2) Sair\n\n");
+	printf(">> ");
+
+	int entrada;
+	scanf("%d", &entrada);
+
+	if(entrada == 1)
+	{
+		n = GenerateMatrixes();
+
+		printf("Multiplicando...\nResultado:\n\n");
+		C = strassenMultiply(A, B, n);
+
+		printMatrix(C, n);
+	}
+	else { exit(EXIT_SUCCESS); }
 
 	return 0;
 }
