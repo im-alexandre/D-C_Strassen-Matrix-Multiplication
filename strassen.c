c0c41aac1d4d84a2bae189967daa63fa03388590
Alexandre Castro <im.alexandre07@gmail.com>
2024-03-09 16:29:47 -0300

    -

--- strassen.c
+++ strassen.c
@@ -1,28 +1,16 @@
-#include "functions.h"
+#include "functions.c"
+
 
 int main(int argc, char const *argv[])
 {
-	int n;
-	system("clear");
+	int n = 9;
 	printf("Multiplicação de Matrizes Quadradas\n\n");
+    GenerateMatrixes(n);
 
-	printf("1) Gerar matrizes\n");
-	printf("2) Sair\n\n");
-	printf(">> ");
+    printf("Multiplicando...\nResultado:\n\n");
+    C = strassenMultiply(A, B, n);
 
-	int entrada;
-	scanf("%d", &entrada);
-
-	if(entrada == 1)
-	{
-		n = GenerateMatrixes();
-
-		printf("Multiplicando...\nResultado:\n\n");
-		C = strassenMultiply(A, B, n);
-
-		printMatrix(C, n);
-	}
-	else { exit(EXIT_SUCCESS); }
+    printMatrix(C, n);
 
 	return 0;
 }
